08a06b984c88a72a8745ff1221cbb3cc26945382
fariz5 <fariz@ruangguru.com>
2021-06-20 20:19:17 +0700

    add test post json

--- src/test/java/com/poc/test/ApiTest.java
+++ src/test/java/com/poc/test/ApiTest.java
@@ -1,8 +1,14 @@
 package com.poc.test;
 
 import org.junit.Test;
+
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
 
 public class ApiTest {
 
@@ -14,4 +20,21 @@ public class ApiTest {
 			statusCode(200).
 			body(matchesJsonSchemaInClasspath("com/poc/test/schema.json"));
 	}
+	
+	@Test
+	public void checkPostResponse() {
+		JsonPath requestData = new JsonPath(new File("src/test/java/com/poc/test/postRequestData.json"));
+		
+		given().
+			contentType("application/json").
+			body(requestData.getJsonObject("")).
+		when().
+			post("https://jsonplaceholder.cypress.io/posts").
+		then().
+			statusCode(201).
+			body("userId", equalTo(requestData.get("userId"))).
+			body("title", equalTo(requestData.get("title"))).
+			body("body", equalTo(requestData.get("body")));
+	}
+	
 }
